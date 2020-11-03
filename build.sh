949ae5702ec3734bb610e7c007a60f4ed233ca41
oltdaniel <53529846+oltdaniel@users.noreply.github.com>
2020-11-04 00:48:23 +0100

    follow filenames guideline
    they request the filename 'Blatt-XX_Tutorium-YY_Gruppe-ZZZ.pdf' which will now be automatically followed.

--- build.sh
+++ build.sh
@@ -27,6 +27,19 @@ if [[ ! -d out ]]; then
     mkdir -p out
 fi
 
+# extract data for the filename
+GRUPPE=$(cat config.tex | grep 'gruppe' | awk 'NR == 3 {print $1}' RS='{' FS='}')
+if [[ $GRUPPE -lt 10 ]]; then
+    GRUPPE="0$GRUPPE"
+fi
+if [[ $GRUPPE -lt 100 ]]; then
+    GRUPPE="0$GRUPPE"
+fi
+TUTORIUM=$(cat config.tex | grep 'tutorium' | awk 'NR == 3 {print $1}' RS='{' FS='}')
+if [[ $TUTORIUM -lt 10 ]]; then
+    TUTORIUM="0$TUTORIUM"
+fi
+
 # for each uebung
 for ueb in $UEBUNGEN; do
     # check if tex file exists 
@@ -41,7 +54,14 @@ for ueb in $UEBUNGEN; do
         log "ERR" "Failed to compile $ueb/main.tex"
         exit 1
     fi
+    # extract further data for the filename
+    UEBUNGSNUMMER=$(cat $ueb/main.tex | grep 'setuebungsnr' | awk 'NR == 2 {print $1}' RS='{' FS='}')
+    if [[ $UEBUNGSNUMMER -lt 10 ]]; then
+        UEBUNGSNUMMER="0$UEBUNGSNUMMER"
+    fi
+    # create filename
+    FILENAME="Blatt-${UEBUNGSNUMMER}_Tutorium-${TUTORIUM}_Gruppe-${GRUPPE}.pdf"
     # copy and cleanup
-    cp $ueb/main.pdf out/$ueb.pdf
+    cp $ueb/main.pdf out/$FILENAME
     rm $ueb/main.pdf $ueb/main.aux $ueb/main.log $ueb/main.out
 done
